3475319734af5886bb0f261263c83d7ed9f8c67c
lamuyen15 <nguyenthilamuyen0105@gmail.com>
2022-10-05 17:20:29 +0700

    Update SinhVien.ts

--- src/SinhVien.ts
+++ src/SinhVien.ts
@@ -1,38 +1,41 @@
-interface Contact{
- id:number;
- name:string; 
- phone?:number;
- gender?:string;
-}
-const contacts:Contact[]=[];
+import { Contact } from './Contact';
+import { IDoSinhVienTask } from './IDoSinhVienTask';
 
-export class SinhVien implements Contact{
-   id:number;
-   name:string;
-   phone?:number;
-   gender?:string;
+export class SinhVien extends Contact implements IDoSinhVienTask {
+  masinhvien: number | undefined;
 
-  constructor(i:number, n:string,p?:number,g?:string){
-    this.name=n;
-    this.id=i;
+  constructor(i: number, n: string, m: number) {
+    super();
+    this.name = n;
+    this.id = i;
+    this.masinhvien = m;
+  }
+  diemChuyenCan(): void {
+    throw new Error('Method not implemented.');
+  }
+  diemTieuLuan(): void {
+    throw new Error('Method not implemented.');
+  }
+  diemCuoiKy(): void {
+    throw new Error('Method not implemented.');
   }
 
-  getName (){
+  getName() {
     return this.name;
   }
- 
-  setName(a:string){
-    if(a.length > 15){ 
-      this.thongBao("Ten qua dai")}
-    else{
+
+  setName(a: string) {
+    if (a.length > 15) {
+      this.thongBao('Ten qua dai');
+    } else {
       this.name = a;
-     this.thongBao("Ten moi la:");
+      this.thongBao('Ten moi la:' + a);
+    }
   }
- }
- private thongBao(tb:string){
-  console.log("Xin thong bao " + tb);
- }
+  private thongBao(tb: string) {
+    console.log('Xin thong bao ' + tb);
+  }
 }
-let s = new SinhVien(1,"uyen");
-console.log(`${s.id},${s.name}`);
-s.setName("nguyenthilamuyen");
+let s = new SinhVien(1, 'uyen', 123);
+console.log(`${s.id},${s.name},${s.masinhvien}`);
+s.setName('lamuyen');
