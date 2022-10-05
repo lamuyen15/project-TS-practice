63775b4aadc42d6ada5e643e6456151fe7ab6ad1
lamuyen15 <nguyenthilamuyen0105@gmail.com>
2022-10-05 11:55:33 +0700

    Update SinhVienDh.ts

--- src/SinhVienDh.ts
+++ src/SinhVienDh.ts
@@ -1,30 +1,28 @@
-import { SinhVien } from "./SinhVien";
+import { SinhVien } from './SinhVien';
 
-class SinhVienDaiHoc extends SinhVien{
-    soNamHoc(){
-        console.log("4 nam");
-    }
-    tongDiem(numberOne:number, numberTwo:number){
-    return numberOne+numberTwo;
-    }
+class SinhVienDaiHoc extends SinhVien {
+  soNamHoc() {
+    console.log('4 nam');
+  }
+  tongDiem(numberOne: number, numberTwo: number) {
+    return numberOne + numberTwo;
+  }
 }
-let sv = new SinhVienDaiHoc(1,"uyen");
+let sv = new SinhVienDaiHoc(1, 'uyen');
 sv.soNamHoc();
-console.log(sv.tongDiem(12,12));
+console.log(sv.tongDiem(12, 12));
 
-class DuHS extends SinhVienDaiHoc{
-    quocTich(){
-        console.log(svnn.name +  "VietNam");
-    }
-    setName(n:string){
-        this.name=n;
-    }
-   
+class DuHS extends SinhVienDaiHoc {
+  quocTich() {
+    console.log(svnn.name + 'VietNam');
+  }
+  setName(n: string) {
+    this.name = n;
+  }
 }
-let svnn = new DuHS(2,"mina ");
+let svnn = new DuHS(2, 'mina ');
 svnn.quocTich();
 svnn.soNamHoc;
-svnn.setName("Lam")
+svnn.setName('Lam');
+console.log(svnn.id);
 console.log(svnn.getName());
-
-
